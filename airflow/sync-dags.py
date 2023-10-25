12a4d6e3a1798ad709d7faf64af4a8b1e70973d9
Wilis Cavalcante <56607831+wiliscavalcante@users.noreply.github.com>
2023-10-25 03:10:32 -0300

    Update sync-dags.py

--- airflow/sync-dags.py
+++ airflow/sync-dags.py
@@ -3,6 +3,7 @@ import subprocess
 import datetime
 import boto3
 import shutil
+from tqdm import tqdm
 
 PVC_DIRECTORY = '/home/ubuntu/projetos/airflow/pvc'
 TEMP_DIRECTORY = '/home/ubuntu/projetos/airflow/temp'
@@ -29,7 +30,18 @@ def get_timestamp(iso_date):
             print(f"\nErro: data inválida no formato: '{iso_date}'. O formato esperado é '%Y-%m-%dT%H:%M:%S+00:00'.")
             return None
 
-# Obtendo a lista de pastas DAG do S3
+def download_with_progress(bucket, key, filename):
+    s3 = boto3.resource('s3')
+    meta_data = s3.meta.client.head_object(Bucket=bucket, Key=key)
+    total_size = int(meta_data.get('ContentLength', 0))
+    progress = tqdm(total=total_size, unit='B', unit_scale=True, desc=filename)
+
+    def callback(bytes_transferred):
+        progress.update(bytes_transferred)
+
+    s3.meta.client.download_file(bucket, key, filename, Callback=callback)
+    progress.close()
+
 response = s3.list_objects_v2(Bucket=S3_BUCKET)
 s3_folders = set()
 for content in response.get('Contents', []):
@@ -40,7 +52,6 @@ for folder in s3_folders:
     local_file = f"{TEMP_DIRECTORY}/{folder}.zip"
     pvc_folder = f"{PVC_DIRECTORY}/{folder}"
 
-    # Obter a hora da última modificação do arquivo no S3
     response = s3.head_object(Bucket=S3_BUCKET, Key=s3_file)
     s3_time = response['LastModified']
     s3_seconds = get_timestamp(s3_time.strftime('%Y-%m-%dT%H:%M:%S+00:00'))
@@ -48,27 +59,29 @@ for folder in s3_folders:
     if s3_seconds is None:
         continue
 
-    # Verificar se o arquivo já foi baixado anteriormente
     recorded_time = None
     if os.path.exists(pvc_folder):
         recorded_time = os.path.getmtime(pvc_folder)
     recorded_seconds = recorded_time if recorded_time else 0
 
-    # Baixar o arquivo se ele for mais novo ou se a pasta DAG não existir
     if s3_seconds > recorded_seconds or not os.path.exists(pvc_folder):
         print(f"\nBaixando s3://{S3_BUCKET}/{s3_file} para {local_file}.")
         try:
-            s3.download_file(S3_BUCKET, s3_file, local_file)
+            download_with_progress(S3_BUCKET, s3_file, local_file)
             print(f"\nDownload bem-sucedido de s3://{S3_BUCKET}/{s3_file}.")
 
-            # Descompactar o arquivo
             if os.path.exists(pvc_folder):
                 shutil.rmtree(pvc_folder)
             os.makedirs(pvc_folder, exist_ok=True)
             unzip_command = f"unzip -q {local_file} -d {pvc_folder}"
             os.system(unzip_command)
 
-            # Atualizar a hora de modificação da pasta DAG
+            # Remover o arquivo .zip após a descompactação
+            try:
+                os.remove(local_file)
+            except OSError as e:
+                print(f"Erro ao remover {local_file}. Erro: {e.strerror}")
+
             os.utime(pvc_folder, (s3_seconds, s3_seconds))
 
         except Exception as e:
